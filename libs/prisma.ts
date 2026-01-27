039870597917224f07c0622fa009e2a89d10141c
RHamzaZulfiqarJ <hamzazulfiqar123123@gmail.com>
2026-01-28 00:18:11 +0500

    Adjusted prisma according to production

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -2,21 +2,20 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
